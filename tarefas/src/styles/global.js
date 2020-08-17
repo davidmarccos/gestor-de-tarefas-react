73dd3bd9e8812a1230bb6f4cc5b1b6b7c5572d22
David Marcos <david.mrcs@gmail.com>
2020-08-17 17:32:05 -0300

    INICIO

---
+++ tarefas/src/styles/global.js
@@ -0,0 +1,6 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
+
+`;
